86160cd336c370d27f2a03f18408b64e6470b435
Archana-Bhamare <archusonar2@gmail.com>
2021-01-03 16:53:17 +0530

    Added : JWT token configured

---
+++ tokens.py
@@ -0,0 +1,16 @@
+import datetime
+
+from FundooNote_Project.settings import SECRET_KEY
+
+import jwt
+
+
+def token_activation(username, password):
+    data = {
+        'username': username,
+        'password': password,
+        'exp': datetime.datetime.now() + datetime.timedelta(days=1)
+    }
+
+    token = jwt.encode(data, SECRET_KEY, algorithm="HS256").decode('utf-8')
+    return token
